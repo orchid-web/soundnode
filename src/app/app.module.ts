9b8e32289992fb51054a151b00d98fe10f5bcbeb
Jonathan Bertholom <j-bertholom@live.fr>
2019-09-02 16:40:46 +0200

    creation menu et composants pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { LikesComponent } from './likes/likes.component';
+import { PlaylistsComponent } from './playlists/playlists.component';
+import { StreamComponent } from './stream/stream.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    LikesComponent,
+    PlaylistsComponent,
+    StreamComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
