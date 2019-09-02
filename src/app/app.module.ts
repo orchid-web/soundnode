6249cb41a8d2008285805daa1211155badb292c3
Christophe <noctis_59@yahoo.fr>
2019-09-02 13:36:45 +0200

    Creation component Login + mise en page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignInComponent
+    SignInComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
