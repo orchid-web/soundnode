b7545a270b6297d82f6a5ddab6976142ed93f141
Christophe <noctis_59@yahoo.fr>
2019-09-03 10:06:49 +0200

    mise en place affichage formulaire login en cliquant sur le lien prévu à cette effet

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { faCloud } from '@fortawesome/free-solid-svg-icons';
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { faBookmark } from '@fortawesome/free-solid-svg-icons';
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
+
 export class MenuComponent implements OnInit {
   faCloud = faCloud;
   faAngleLeft = faAngleLeft;
@@ -18,9 +21,9 @@ export class MenuComponent implements OnInit {
   faBookmark = faBookmark;
   faUserCircle = faUserCircle;
   linkLog = false;// Affiche login
-  // @Output() test = 5;
+  @Output() log:EventEmitter<Boolean> = new EventEmitter();
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
@@ -28,11 +31,14 @@ export class MenuComponent implements OnInit {
   logIn = () => {
     alert('connexion');
     this.linkLog = true;
+    this.log.emit(this.linkLog);
   }
 
   logOut = () => {
     alert('Deconnexion');
     this.linkLog = false;
+    this.log.emit(this.linkLog);
+    this.router.navigate(['/'])
   }
 
 }
